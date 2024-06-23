16a6229d176d99f84268825f8330f70cfa0b3a5f
redacoda <reda.arkha@gmail.com>
2024-06-23 14:27:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('more')
+
+button.onclick = function() {
+    console.log('Button clicked');
+    window.location.href = 'https://github.com/redacoda?tab=repositories';
+};
